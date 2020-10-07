2a336475e756df5a0be19207b0d795f6a71548b4
n0y0j <n0y0j77@gmail.com>
2020-10-07 22:24:01 +0900

    feat: Implement bottom_navigation_var using fragment

--- app/src/main/java/com/example/shoppingmall/MainActivity.java
+++ app/src/main/java/com/example/shoppingmall/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.shoppingmall;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.shoppingmall.Fragment.Buy;
+import com.example.shoppingmall.Fragment.Favorite;
+import com.example.shoppingmall.Fragment.Home;
+import com.gauravk.bubblenavigation.BubbleNavigationLinearView;
+import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentTransaction fragmentTransaction;
+    BubbleNavigationLinearView bubbleNavigationLinearView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        if(Build.VERSION.SDK_INT >= 21) {
+//            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+//            Window window = getWindow();
+//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//            window.setStatusBarColor(Color.TRANSPARENT);
+//        }
+
+        // 액션바 사용 안함
+        getSupportActionBar().hide();
+
+        bubbleNavigationLinearView = findViewById(R.id.bottom_nav);
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new Home());
+        fragmentTransaction.commit();
+
+        bubbleNavigationLinearView.setNavigationChangeListener(new BubbleNavigationChangeListener() {
+            @Override
+            public void onNavigationChanged(View view, int position) {
+
+                switch (position) {
+                    case 0:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.fragment_container, new Home());
+                        fragmentTransaction.commit();
+                        break;
+                    case 1:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.fragment_container, new Favorite());
+                        fragmentTransaction.commit();
+                        break;
+                    case 2:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.fragment_container, new Buy());
+                        fragmentTransaction.commit();
+                        break;
+                }
+            }
+        });
+
     }
 }
