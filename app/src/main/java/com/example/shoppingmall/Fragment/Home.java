5aebbbffd56bb1893ba39bb37e6f7d0664a1278f
n0y0j <n0y0j77@gmail.com>
2020-10-08 20:38:10 +0900

    feat: Use Crawling to store programming languages
    Create a list that uses crawling to store popular programming languages

--- app/src/main/java/com/example/shoppingmall/Fragment/Home.java
+++ app/src/main/java/com/example/shoppingmall/Fragment/Home.java
@@ -19,15 +19,14 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class Home extends Fragment {
 
-    final StringBuilder builder = new StringBuilder();
-    String[] items = new String[10];
-
     // AsyncTask<doInBackground()의 변수 종류, onProgressUpdate()에서 사용할 변수 종류, onPostExecute()에서 사용할 변수종류>
-    private class CodeCrawling extends AsyncTask<String, Void, Void> {
+    //           execute시 사용하는 인자의 변수 종류, 변하는 값의 변수 종류, return 값의 변수 종류
+    private class CodeCrawling extends AsyncTask<String, Void, ArrayList<String>> {
 
         // 백그라운드 작업이 시작되기 전 호출
         // 메소드 준비작업을 하는 장소
@@ -38,29 +37,39 @@ public class Home extends Fragment {
 
         // 실제 동작하는 부분
         @Override
-        protected Void doInBackground(String... url) {
+        protected ArrayList<String> doInBackground(String... url) {
+            // 크롤링한 데이터를 저장하고 return할 리스트
+            ArrayList<String> items = new ArrayList<String>();
 
             try {
+                // TIOBE 사이트의 HTML 정보를 가져옴
                 Document doc = Jsoup.connect(url[0]).get();
+                // table -> tbodt -> tr 태그의 정보를 저장
                 Elements elements = doc.select("table tbody tr");
 
-                Log.d("TAG", doc.toString());
-
+                // 많이 사용되는 언어의 1위부터 10위까지의 정보만 저장
+                int count = 0;
+                for(Element elem : elements) {
+                    if (count < 10) {
+                        String item_temp = elem.select("td").text();
+                        // String을 Slicing하여 사용할 정보만 저장함 ( 언어 이름 )
+                        String[] item = item_temp.replace("  ", " ").split(" ");
+                        count++;
+                        items.add(item[2]);
+                    }
+                    else break;
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            return null;
+            return items;
         }
 
-        // 사용자에게 진행을 알릴때 사용
-        @Override
-        protected void onProgressUpdate(Void... values) {
-        }
 
         // 백그라운드 작업이 완료 후 결과값을 얻는다.
         @Override
-        protected void onPostExecute(Void aVoid) {
+        protected void onPostExecute(ArrayList<String> aVoid) {
             super.onPostExecute(aVoid);
         }
     }
@@ -78,7 +87,16 @@ public class Home extends Fragment {
 
         CodeCrawling c1 = new CodeCrawling();
 
-        c1.execute("https://www.tiobe.com/tiobe-index/");
+        ArrayList<String> code = new ArrayList<String>();
+        try {
+            code = c1.execute("https://www.tiobe.com/tiobe-index/").get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Log.d("TAG", code.get(0));
 
 
 
